a7bc8f57c4e0235a3108a75bc10062ec7186edbf
rizzatoo <141828701+rizzatoo@users.noreply.github.com>
2024-10-25 08:33:32 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -6,8 +6,8 @@ criaCartao(
 
 criaCartao(
     'Esportes',
-    'O que é o Voleibol?',
-    'É um esporte de popularidade significativa em grande parte do mundo, e está presente em muitos torneios e eventos esportivos de âmbito internacionais, tais como os Jogos Olímpicos e os Jogos Panamericanos.'
+    'Como funciona a Ginastica Olímpica?',
+     'Ginástica olímpica é uma modalidade de ginástica onde os atletas realizam um conjunto de exercícios em aparelhos oficiais. Movimentos estes que revelam força, agilidade, flexibilidade, coordenação, equilíbrio e controle do corpo.'
 )
 
 criaCartao(
